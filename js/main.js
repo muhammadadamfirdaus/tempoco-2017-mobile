a82000e2e072381926d8f503f7343cd65c3e855f
muhammadadamfirdaus <adam@muhammadadamfirdaus>
2017-05-13 05:09:28 +0700

    update

--- js/main.js
+++ js/main.js
@@ -38,11 +38,11 @@ $(function(){
 	});
 
 	// Go To
-	// $('a[href^="#"]').click(function() {
-	// 	$('html,body').animate({
+	// $('a[href^="#"].scroll').on('click', function(e){
+	// 	e.preventDefault();
+	// 	$('article.wrapper').animate({
 	// 		scrollTop: $(this.hash).offset().top
-	// 	}, 1000);
-	// 	return false;
+	// 	}, 2000);
 	// });
 
 	// RESPONSIVE STUFF
@@ -77,9 +77,7 @@ $(function(){
 			centeredSlides: true,
 			loopedSlides: 3, //looped slides should be the same
 			touchRatio: 0.2,
-			preventClicks: false,
-			preventClicksPropagation: false,
-			slideToClickedSlide: true,
+			slideToClickedSlide: true
 	});
 	var mobileContent = new Swiper('main .content', {
 			spaceBetween: 10,
@@ -89,12 +87,16 @@ $(function(){
 			hashnav: true,
 			hashnavWatchState: true,
 			grabCursor: true,
+			onAfterResize: function(){
+				mobileNavigation.update(true);
+				mobileContent.update(true);
+			}
 	});
 	mobileNavigation.params.control = mobileContent;
 	mobileContent.params.control = mobileNavigation;
 	/* end mobile swipe */
 
-	$('.swiper-slide a').on('click', function(){
+	$('.swiper-slide > a, .swiper-slide a[href^="#"]').on('click', function(){
 		window.location.href = $(this).attr("href");
 	});
 
@@ -243,6 +245,50 @@ $(function(){
 	// 	$('.tab-content:nth-of-type(1)').addClass('tab-active');
 	// }
 	/* end tab */
+
+	/* detail article only */
+	if($('article').length){
+		/* onscroll */
+		$('.scroll-container').on('scroll', function(){
+			/* check element to bottom */
+			if($(this).scrollTop() + $(this).innerHeight() >= $(this)[0].scrollHeight){
+        // $('.bottom-nav ul').addClass('active');
+    	} else {
+    		// $('.bottom-nav ul').removeClass('active');
+    	}
+		});
+		/* end onscroll */
+	}
+	/* end detail article only */
+
+	/* detail foto */
+	if($('#detail-foto').length){
+		$('head').append('<script src="js/photoswipe.min.js"></script><link rel="stylesheet" type="text/css" href="css/photoswipe.css"><link rel="stylesheet" type="text/css" href="css/photoswipe-default-skin.css">');
+		if($('script[src="js/photoswipe.min.js"]').length){
+			console.log('ada jsnya');
+			// Photoswipe
+			if($('figure').length){
+				var slideSelector = 'figure img',
+		    options     = {bgOpacity: 0.8},
+		    events      = {
+		        close: function () {
+		            console.log('closed');
+		        }
+		    };
+				$('article').photoSwipe(slideSelector, options, events);
+			}
+		}
+	}
+	/* end detail foto */
+
+	// Social Plugin Button po.st
+	var s = document.createElement('script');
+	s.type = 'text/javascript';
+	s.async = true;
+	s.src = ('https:' == document.location.protocol ? 'https://s' : 'http://i')
+	+ '.po.st/static/v4/post-widget.js#publisherKey=g2s58c5cupgtrsulvrd0';
+	var x = document.getElementsByTagName('script')[0];
+	x.parentNode.insertBefore(s, x);
 
 	// Google Analytics
 	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
